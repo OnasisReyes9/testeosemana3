62dd32838f580486b5ab197d78090aef63945364
KysTyler <62519436+KysTyler@users.noreply.github.com>
2020-08-06 21:58:34 -0600

    Adding random stuff

--- JavaApplication188/src/javaapplication188/Tyler_Class.java
+++ JavaApplication188/src/javaapplication188/Tyler_Class.java
@@ -12,5 +12,18 @@ package javaapplication188;
 public class Tyler_Class {
     public void nothing(){
         System.out.println("Hello Onasis");
+        System.out.println("This  world");
+        try {
+            System.out.println("Hello from this PC world to the next");
+            for (int i = 0; i < 10; i++) {
+                System.out.println("Ojala no nos pisen manana");
+            }
+            int num = 0;
+            while (num < 50) {
+                System.out.println("Tengo miedo");
+                num++;
+            }
+        } catch (Exception e) {
+        }
     }
 }
